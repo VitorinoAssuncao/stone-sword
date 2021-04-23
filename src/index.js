26af7c1ab824e55105e1e37db5c86acb1d03cab1
VitorinoAssuncao <47284469+VitorinoAssuncao@users.noreply.github.com>
2021-04-23 02:43:09 -0300

    add rotas
    adicionadas rotas para pesquisa por personagem da marvel e para pagina de usuario

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import './index.css';
 
 import App from './pages/App';
 import User from './pages/User';
+import MarvelCharacther from './pages/Marvel-Characthers';
+import store from './redux/store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Switch>
-      <Route path="/" exact={true} component={App} />
-      <Route path="/user" exact={true} component={User} />
-    </Switch>
+      <Switch>
+        <Route path="/" exact={true} component={App} />
+        <Route path="/user" exact={true} component={User} />
+        <Route path="/characthers/search" exat={true} component={MarvelCharacther} />
+      </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
