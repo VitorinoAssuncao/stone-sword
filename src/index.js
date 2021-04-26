71a8ed2271004d874da9b3a7a20ecfa271cddf01
VitorinoAssuncao <47284469+VitorinoAssuncao@users.noreply.github.com>
2021-04-26 01:25:41 -0300

    add rotas
    adicionadas rotas para os links das paginas, e corrigida palavra

--- src/index.js
+++ src/index.js
@@ -6,15 +6,22 @@ import './index.css';
 
 import App from './pages/App';
 import User from './pages/User';
-import MarvelCharacther from './pages/Marvel-Characthers';
-// import store from './redux/store';
+import UserRegistry from './pages/User-Registry';
+import MyCharacter from './pages/My-Characters';
+import MyComics from './pages/My-Comics';
+import MarvelCharacter from './pages/Marvel-Characters';
+import MarvelComic from './pages/Marvel-Comics';
 
 ReactDOM.render(
   <BrowserRouter>
       <Switch>
         <Route path="/" exact={true} component={App} />
         <Route path="/user" exact={true} component={User} />
-        <Route path="/characthers/search" exat={true} component={MarvelCharacther} />
+        <Route path="/user/characters" exact={true} component={MyCharacter} />
+        <Route path="/user/comics" exact={true} component={MyComics} />
+        <Route path="/user/registry" exact={true} component={UserRegistry} />
+        <Route path="/characters/search" exat={true} component={MarvelCharacter} />
+        <Route path="/comics/search" exat={true} component={MarvelComic} />
       </Switch>
   </BrowserRouter>,
   document.getElementById('root')
