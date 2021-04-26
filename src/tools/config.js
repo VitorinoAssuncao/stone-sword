f48a68baff15465d41eba860898165718d215be6
VitorinoAssuncao <47284469+VitorinoAssuncao@users.noreply.github.com>
2021-04-26 01:24:06 -0300

    del chave privada
    retirada chave privada

--- src/tools/config.js
+++ src/tools/config.js
@@ -1,6 +1,5 @@
 var ts = "abc"
 var apikey = "a5d7b8ac4b607ad127aa30321185119d"
-var privatekey = "23279f2fff3927a433c8e2f5964d8fd2f518eb31"
 var hash = "59237c91f07013c39a379a975573fa09"
 var marvel_key = "ts="+ts+"&apikey="+apikey+"&hash="+hash
 export default marvel_key;
