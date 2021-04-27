333cd64b8774337a44e58bb426a1cc1b7ea90e18
VitorinoAssuncao <47284469+VitorinoAssuncao@users.noreply.github.com>
2021-04-27 01:14:44 -0300

    My-Characther
    finalizada a estrutura de geracao de personagens favoritos

--- src/pages/My-Characters.js
+++ src/pages/My-Characters.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react';
+import {Link} from 'react';
 import Sidebar from '../components/Sidebar';
 import SidebarItem from '../components/SidebarItem';
 import Main from '../components/Main';
-import FavoriteButton,{sendFavorite} from '../components/FavoriteButton'
+import FavoriteButton,{removeFavorite,sendFavorite} from '../components/FavoriteButton'
 
 import '../index.css';
 import marvel_key from '../tools/config';
@@ -11,11 +12,14 @@ class MyCharacter extends React.Component {
   constructor(props){
     super(props);
     this.state = {'character_list':{}};
-    this.state = {'marvel_data':[]};
+    this.marvel_data = [];
+    this.state = {'body':{}};
+
+    this.renderList = this.renderList.bind(this);
+    this.renderCharacter = this.renderCharacter.bind(this);
   }
 
   user_id = sessionStorage.getItem('@stone-sword/id');
-  get_marvel_data = sessionStorage.getItem('@stone-sword/marvel_data');
 
   componentDidMount(){
     const requestOptions = {
@@ -25,26 +29,91 @@ class MyCharacter extends React.Component {
 
     fetch(`https://stone-shield.herokuapp.com/users/${this.user_id}/characthers`, requestOptions)
     .then(res => res.json())
-    .then(res  => this.setState({character_list: res}));
+    .then(res => this.setState({favorite_list: res}))
+    .then(res => this.renderList(this.state.favorite_list))
+
   };
 
-  renderList(item_list){
-    if(item_list !== undefined){
-      item_list.forEach((item,index) =>
-      this.takeDataFromMarvel(item.marvel_id)             
-        );
-    }
-  }
 
-  takeDataFromMarvel(marvel_id){
-    var temp_data = {}
+  renderList(item_list){
     const requestOptions = {
       method : 'GET'
     }
-    fetch(`https://gateway.marvel.com/v1/public/characters/${marvel_id}?${marvel_key}`, requestOptions)
-    .then(res => res.json())
-    .then(res => temp_data = res.data.results);
-  }
+
+    if(item_list !== undefined){
+      item_list.forEach((item,index) =>{
+        fetch(`https://gateway.marvel.com/v1/public/characters/${item.marvel_id}?${marvel_key}`, requestOptions)
+        .then(res => res.json())
+        .then(res => {
+          if(res.status === "Ok"){
+            this.marvel_data.push(res.data.results[0])
+          }
+            else{
+              console.log('errou')
+            };
+            }
+        );
+        }
+      )
+    }
+    }
+
+    renderComics = (data) => {
+      if (data) {
+        var mapRows = data.map((item,index) => (
+          <Fragment key={item.index}>
+                <li>
+                  <b>Título:</b> {item.name}, <a className="text-danger" href={item.resourceURI + "?"+marvel_key}>Link</a>
+                </li>
+          </Fragment>
+        ));
+        return mapRows;
+      };
+    }
+
+    createFavoriteButton(favorite_list,item_id){
+      if(favorite_list !== undefined){
+        var result =this.evalCharacterID(favorite_list,item_id) 
+        if (result.follow === 'follow'){
+          return <FavoriteButton color="success fas fa-heart w-25" type="submit" value=" Favoritar"  click={() =>sendFavorite(this.user_id,item_id,'character')} />
+          }
+        else {
+          return <FavoriteButton color="danger fas fa-minus-circle w-25" type="submit" value=" Remover"  click={() =>removeFavorite(result.id,'character')} />
+          };
+        }
+    }
+
+    evalCharacterID(favorite_list,item_id) {
+      var result  = {follow:'follow',id:0}
+      if (favorite_list !== undefined){
+        favorite_list.forEach((item,index) =>
+          {item.marvel_id === item_id?(result = {follow: 'unfollow',id:item.id}):(console.log(''))}     
+        )
+      }
+      return result
+    }
+
+    renderCharacter = (data) =>{
+      if (data){
+        var mapRows = data.map((item,index) => (
+          <Fragment  key={item.id}>
+          <div className="card bg-light p-2 m-1">
+              <img className="border border-secondary rounded w-25 float-left" alt={item.name} src={`${item.thumbnail.path }/portrait_medium.jpg`} />
+              <p><b>Nome Personagem:</b> {item.name}</p>
+              <p><b>Descrição: </b> 
+              {item.description ?
+              (item.description):("Sem descrição informada")
+              }</p>
+              {this.createFavoriteButton(this.state.favorite_list,item.id)}
+              <hr />
+              <h3>Listagem de Quadrinhos</h3>
+              {item.comics.available > 0 ?(this.renderComics(item.comics.items)):("Sem quadrinhos informados.")}
+          </div>
+          </Fragment>
+        ));
+        return mapRows;
+    }
+    };
 
   render () {
     return (
@@ -69,8 +138,7 @@ class MyCharacter extends React.Component {
         </Sidebar>
 
         <Main>
-          {/* {this.renderList(this.state.character_list)} */}
-          {console.log(this.takeDataFromMarvel(1017297))}
+        {this.renderCharacter(this.marvel_data)}
         </Main>
       </div>
     </div>
